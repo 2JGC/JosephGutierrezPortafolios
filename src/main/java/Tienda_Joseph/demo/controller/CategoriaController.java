2edaa3173c2dc2a73e8cf6323b7fac5a2a9fa200
2JGC <josephgutierrez440@gmail.com>
2026-03-11 18:31:11 -0600

    Semana 06
    Profe subi semana 6 pero con nombre en semana 5

--- src/main/java/Tienda_Joseph/demo/controller/CategoriaController.java
+++ src/main/java/Tienda_Joseph/demo/controller/CategoriaController.java
@@ -4,12 +4,22 @@
  */
 package Tienda_Joseph.demo.controller;
 
+import Tienda_Joseph.demo.domain.Categoria;
 import Tienda_Joseph.demo.service.CategoriaService;
+import jakarta.validation.Valid;
+import java.util.Locale;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/categoria")
@@ -25,6 +35,52 @@ public class CategoriaController {
         model.addAttribute("totalCategorias", categorias.size());
         return "categoria/listado";
     }
+
+    @GetMapping("/nuevo")
+public String nuevo(Model model) {
+    model.addAttribute("categoria", new Categoria());
+    return "categoria/modifica";
 }
 
-    
+    @Autowired
+    private MessageSource messageSource;
+
+    @PostMapping("/guardar")
+    public String guardar(@Valid Categoria categoria, @RequestParam MultipartFile imagenFile, RedirectAttributes redirectAttributes) {
+        categoriaService.save(categoria, imagenFile);
+        redirectAttributes.addFlashAttribute("todoOk", messageSource.getMessage("mensaje.actualizado", null, Locale.getDefault()));
+        return "redirect:/categoria/listado";
+    }
+
+    @PostMapping("/eliminar")
+    public String eliminar(@RequestParam Integer idCategoria, RedirectAttributes redirectAttributes) {
+        String titulo = "todoOk";
+        String detalle = "mensaje.eliminado";
+        try {
+            categoriaService.delete(idCategoria);
+        } catch (IllegalArgumentException e) {
+            titulo = "error"; // Categoría no existe
+            detalle = "categoria.error01";
+        } catch (IllegalStateException e) {
+            titulo = "error"; // Datos asociados impiden eliminar
+            detalle = "categoria.error02";
+        } catch (Exception e) {
+            titulo = "error"; // Excepción inesperada
+            detalle = "categoria.error03";
+        }
+        redirectAttributes.addFlashAttribute(titulo, messageSource.getMessage(detalle, null, Locale.getDefault()));
+        return "redirect:/categoria/listado";
+    }
+
+    @GetMapping("/modificar/{idCategoria}")
+    public String modificar(@PathVariable("idCategoria") Integer idCategoria, Model model, RedirectAttributes redirectAttributes) {
+        Optional<Categoria> categoriaOpt = categoriaService.getCategoria(idCategoria);
+        if (categoriaOpt.isEmpty()) {
+            redirectAttributes.addFlashAttribute("error", messageSource.getMessage("categoria.error01", null, Locale.getDefault()));
+            return "redirect:/categoria/listado";
+        }
+        model.addAttribute("categoria", categoriaOpt.get());
+        return "/categoria/modifica";
+    }
+
+}
