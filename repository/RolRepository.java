8e6d022648a0e208de0d14128a8e4122d3f25969
2JGC <josephgutierrez440@gmail.com>
2026-04-05 01:24:37 -0600

    semana11

--- repository/RolRepository.java
+++ repository/RolRepository.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package Tienda_Joseph.demo.repository;
 
-/**
- *
- * @author josep
- */
-public interface RolRepository {
-    
+import Tienda_Joseph.demo.domain.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    public Optional<Rol> findByRol(String rol);
 }
