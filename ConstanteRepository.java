7ac3e7ba85d1ab0c632ec15e1a4cc2a43cf55ada
2JGC <josephgutierrez440@gmail.com>
2026-04-12 12:18:48 -0600

    pruebas de semana13

---
+++ ConstanteRepository.java
@@ -0,0 +1,12 @@
+
+package Tienda_Joseph.demo.repository;
+
+import Tienda_Joseph.demo.domain.Constante;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConstanteRepository extends JpaRepository<Constante,Integer> {
+    
+    public Optional<Constante> findByAtributo(String atributo);
+    
+}
